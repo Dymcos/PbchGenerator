7c332b927433a521690bb59f96a91762437b74cd
Valentin Dymchenko <dymcos2@gmail.com>
2024-07-12 14:05:55 +0300

    Added minor commentary

--- PbchGenerator.m
+++ PbchGenerator.m
@@ -7,9 +7,9 @@ classdef PbchGenerator
         function bitSequence = generatePbch(MIB,SFN,HRF, SSB_Bits, NCellId, Lmax_)
             arguments
                 MIB (1,24) % MIB binary sequence (MSB to LSB)
-                SFN (1,1) % system frame number positive integer 
+                SFN (1,1) % system frame number positive integer (0...1023)
                 HRF (1,1) % half frame bit
-                SSB_Bits (1,3) % [<MSB of Kssb> 0 0] if Lmax_ =4|8 
+                SSB_Bits (1,3) % [<MSB of Kssb> 0 0] if Lmax_ = 4|8 
                 % or [<6th bit> <5th bit> <4th bit>] of candidate SS/PBCH block index if Lmax_ = 64
                 NCellId (1,1) % cell identificator (0...1007)
                 Lmax_ (1,1) % = 4|8|64 maximum number of candidate SS/PBCH blocks in half frame [4.1, TS 38.213]
